271b75efdd8f677d18d0ea4a013608baa44c24f5
Eduardo O. Santos <75816516+Eduardo-O-Santos@users.noreply.github.com>
2023-12-07 17:36:11 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="text-orange-500">
+        <h1>Boilerplate React + Tailwindcss</h1>
+      </div>
+    </>
+  );
+}
+
+export { App };
